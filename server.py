c0de988c301f0ac8e941d208c4347d0eb35366f7
João pedro oliveira <joaogrilo265@gmail.com>
2025-02-02 14:07:21 -0300

    add port and host=0.0.0.0

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from waitress import serve
 from Task_list.wsgi import application
+import os
 
 if __name__ == "__main__":
-    serve(application, host="127.0.0.1", port=8000)
+    # Isso pega a porta fornecida pelo Render via a variável de ambiente PORT, ou usa a porta 8000 como fallback, caso a variável não esteja presente.
+    port = os.getenv("PORT", 8000)
+    # host="0.0.0.0": Isso garante que a aplicação aceite conexões de qualquer interface de rede, permitindo que o Render acesse a aplicação de fora do container.
+    serve(application, host="0.0.0.0", port=port)
